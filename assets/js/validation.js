253d01672685a04112f5556bab7d959d8b14fdad
Kristofer Martin R. Pillarina <41129289+CodexApple@users.noreply.github.com>
2021-11-04 21:24:48 +0800

    Added SweetAlerts

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -6,17 +6,13 @@
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
-
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
-    }
-
-    tablinks = document.getElementsByClassName("tablinks");
+    } tablinks = document.getElementsByClassName("tablinks");
 
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
@@ -27,11 +23,27 @@ function Validate(evt, tabName) {
     const orderNumber = $("#orderValid").val();
     const mobileNumber = $("#mobileValid").val();
 
+    var Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true
+    });
+
     if (validateInput(email, phoneNumber, mobileNumber, orderNumber)) {
-        openTab(evt, tabName);
+        openTab(evt, tabName)
+        Toast.fire({
+            icon: 'success',
+            title: 'Details has been accepted'
+        });
     } else {
-        alert('You have entered an invalid input, Data you have placed\n\n' + '\nEmail: ' + email + '\nTelephone Number: ' + 
-            phoneNumber + '\nMobile Number: ' + mobileNumber + '\nOrder Number: ' + orderNumber);
+        Toast.fire({
+            icon: 'error',
+            title: 'You have entered an invalid input, Please check\n' +
+            'Email: ' + email + '\nPhone Number: ' + phoneNumber + '\nMobile Number: ' + mobileNumber +
+            '\nOrder Number: ' + orderNumber
+        });
     }
 }
 
@@ -43,9 +55,5 @@ function validateInput(email, phoneNumber, mobileNumber, orderNumber) {
 
     if (validEmail.test(email) && validPhone.test(phoneNumber) && validMobile.test(mobileNumber) && validOrder.test(orderNumber)) {
         return true;
-    } return false;
-}
-
-function removeInput() {
-    
+    }
 }
